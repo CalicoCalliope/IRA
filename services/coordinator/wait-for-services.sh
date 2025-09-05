80f850922359328906fb783abe3bbe65103dde35
Yaren Durgun <yaren.durgun@ug.bilkent.edu.tr>
2025-09-05 03:50:31 +0200

    preparing for vm: deleted extension folder

---
+++ services/coordinator/wait-for-services.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Usage: ./wait-for-services.sh host1:port host2:port ... -- command args
+
+set -e
+
+# Separate service args from command
+SERVICES=()
+while [ "$1" != "--" ]; do
+  SERVICES+=("$1")
+  shift
+done
+shift
+
+# Wait for each service
+for s in "${SERVICES[@]}"; do
+  host=$(echo $s | cut -d: -f1)
+  port=$(echo $s | cut -d: -f2)
+  echo "Waiting for $host:$port..."
+  while ! nc -z $host $port; do
+    sleep 1
+  done
+done
+
+# Execute command
+exec "$@"
